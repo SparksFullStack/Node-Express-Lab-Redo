9b3adccd31bff997ef6ab37e8a0111efaf40d144
SparksFullStack <csparks@antioch.edu>
2018-09-03 12:56:17 -0700

    React app basic structure and dependencies setup

---
+++ client/src/components/PostList.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class PostList extends Component {
+    componentWillMount(){
+
+    }
+
+    render(){
+        return(
+            <div>
+                <h1>Post List Nibba</h1>
+            </div>
+        )
+    }
+}
+
+export default PostList;
